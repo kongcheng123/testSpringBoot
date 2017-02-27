bb46779284a161d96a940f6bf53498b414e7b31e
xy <xy@xydeMacBook-Pro.local>
2017-02-27 22:06:28 +0800

    修改结构

--- src/main/java/com/test/controller/HelloController.java
+++ src/main/java/com/test/controller/HelloController.java
@@ -6,11 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by xy on 2016/12/22.
@@ -20,7 +16,7 @@ public class HelloController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping("/index")
+    @RequestMapping("/")
     public String index(Model model){
         User u=new User("qwe");
         userDao.save(u);
